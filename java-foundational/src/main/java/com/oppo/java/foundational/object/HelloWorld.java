fec0d2ec391e0befc2e08bd6d7a18b629c32e874
xiefei <walkman159@163.com>
2021-09-03 17:54:17 +0800

    第一个Java

---
+++ java-foundational/src/main/java/com/oppo/java/foundational/object/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.oppo.java.foundational.object;
+
+/**
+ * HelloWorld
+ *
+ * @author xiefei walkman159@163.com
+ * @version 2021/9/3 17:52
+ * @since JDK16
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World ");
+    }
+}
