77127dc6baf72922460d178cfe5ecc624c56a4f4
xiefei <walkman159@163.com>
2021-09-03 18:01:02 +0800

    第一个Java

--- java-foundational/src/main/java/com/oppo/java/foundational/object/HelloWorld.java
+++ java-foundational/src/main/java/com/oppo/java/foundational/object/HelloWorld.java
@@ -9,6 +9,6 @@ package com.oppo.java.foundational.object;
  **/
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World ");
+        System.out.println("Hello World a");
     }
 }
